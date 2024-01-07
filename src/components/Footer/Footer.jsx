baf7da7266901e58b534cc3aba401514430940d6
Helena Sánchez <helena.sangar@gmail.com>
2024-01-07 12:57:39 +0100

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,37 +1,52 @@
-import { Container, Row, Col } from 'react-bootstrap'
-import('./Footer.css')
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Footer.css';
+
+
 
 function Footer() {
-
-
     return (
 
-        <>
-            <div className="Footer">
+        <div className="Footer">
+            <Container className="footer-container">
 
-                <Container className='footer-container'>
+                <Row className="footer-sections">
+                    <Col sm={12} md={4}>
+                        <p className="footer-title">Somos</p>
+                        <hr />
+                        <Link to={'/somos/sobre-mamachama'}>Sobre Mamachama</Link>
+                        <br />
+                        <Link to={'/somos/nuestro-equipo'}>Nuestro equipo</Link>
+                        <br />
+                        <Link to={'/contacta'}>Contacta</Link>
+                    </Col>
+                    <Col sm={12} md={4}>
+                        <p className="footer-title">Hacemos</p>
+                        <hr />
+                        <Link to={'/hacemos/acciones'}>¿Qué hacemos?</Link>
+                        <br />
+                        <Link to={'/hacemos/proyectos'}>Proyectos</Link>
+                    </Col>
+                    <Col sm={12} md={4}>
+                        <p className="footer-title">Colabora</p>
+                        <hr />
+                        <Link to={'/colabora/invisibilizadas'}>Libro "Invisibilizadas"</Link>
+                        <br />
+                        <Link to={'/colabora/catering'}>Catering saludable</Link>
+                    </Col>
 
-                    <Row className='footer-sections'>
-                        <Col>
-                            Somos
-                        </Col>
-                        <Col>
-                            Hacemos
-                        </Col>
-                        <Col>
-                            Colabora
-                        </Col>
-                        <Col>
-                            Contacta
-                        </Col>
-                    </Row>
+                </Row>
 
-                </Container>
+                {/* <Row className="footer-links">
+                    <Col>
+                        <p>Enlaces adicionales u otras secciones del footer</p>
+                    </Col>
+                </Row> */}
 
-            </div>
-        </>
-    )
+            </Container>
+        </div>
+    );
 }
 
-export default Footer
-
+export default Footer;
