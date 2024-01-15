2688f8bc7dd3370d8241ee4c4cc88f3f5dc445c7
Helena Sánchez <helena.sangar@gmail.com>
2024-01-15 14:06:33 +0100

    login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,8 +1,47 @@
+import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/auth.context.jsx"
+import { Form } from "react-bootstrap"
+import { useState, useContext } from "react"
+
+
 function LoginForm() {
+
+
+    const [loginData, setLoginData] = useState({
+        email: '',
+        contraseña: ''
+    })
+
+    const navigate = useNavigate()
+
+    const { authenticateUser, storeToken } = useContext(AuthContext)
+
+    const handleInputChange = e => {
+        const { value, name } = e.target
+        setLoginData({ ...loginData, [name]: value })
+    }
+
+    const { password, email } = loginData
 
     return (
 
-        <h1>loginform</h1>
+        <>
+            <Form>
+
+                <Form.Group className='mb-3' controlId='email'>
+                    <Form.Label className='loginform-label'>Email</Form.Label>
+                    <Form.Control type='email' value={email} onChange={handleInputChange} name='email' />
+                </Form.Group>
+
+                <Form.Group className='mb-3' controlId='password'>
+                    <Form.Label className='loginform-label'>Contraseña</Form.Label>
+                    <Form.Control type="password" />
+
+                </Form.Group>
+
+
+            </Form>
+        </>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './MainApp.jsx'
+import { AuthProviderWrapper } from './contexts/auth.context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <AuthProviderWrapper>
+      <Router>
+        <App />
+      </Router>
+    </AuthProviderWrapper>
   </React.StrictMode>,
 )
