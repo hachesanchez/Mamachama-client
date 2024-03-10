2a05ad2389251b0f08afab53d402af005467cfed
Helena Sánchez <helena.sangar@gmail.com>
2024-03-10 19:40:57 +0100

    signupform uppercase - css

--- src/components/ProfileComponents/SignupModal.jsx
+++ src/components/ProfileComponents/SignupModal.jsx
@@ -1,5 +1,5 @@
 import { Modal } from 'react-bootstrap';
-import SignupForm from './Signupform.css';
+import SignupForm from './Signupform';
 
 
 function SignupModal({ showModal, setShowModal, handleAddUser }) {
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,12 +3,12 @@ import AboutSection from '../../components/HomeComponents/AboutSection';
 import NewsSection from '../../components/HomeComponents/NewsSection';
 import ContactSection from '../../components/HomeComponents/ContactSection';
 import POVSection from '../../components/HomeComponents/POVSection';
-import './HomePage.css';
 import testparallaxpink from '../../assets/images/backgroundF5BFC3.png';
 import testparallaxgrey from '../../assets/images/backgroundEFEFEF.png';
 import testparallaxhero from '../../assets/images/mamachama-hero.jpeg';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import { useParallax } from 'react-scroll-parallax';
+import './HomePage.css';
 
 
 function HomePage() {
