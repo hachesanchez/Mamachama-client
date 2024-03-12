c4c785a574920d810afeb301d6aa4cd451c0baec
Helena Sánchez <helena.sangar@gmail.com>
2024-03-12 09:35:43 +0100

    no parallas provider

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -4,17 +4,17 @@ import AppRoutes from './routes/AppRoutes'
 import Navigation from './components/Navigation/Navigation'
 import Footer from './components/Footer/Footer'
 import ScrollToTop from './ScrollToTop'
-import { ParallaxProvider } from 'react-scroll-parallax'
+// import { ParallaxProvider } from 'react-scroll-parallax'
 
 
 function MainApp() {
   return (
     <>
       <ScrollToTop>
-        <ParallaxProvider>
-          <Navigation />
-          <AppRoutes />
-        </ParallaxProvider>
+        {/* <ParallaxProvider> */}
+        <Navigation />
+        <AppRoutes />
+        {/* </ParallaxProvider> */}
         <Footer />
       </ScrollToTop>
     </>
